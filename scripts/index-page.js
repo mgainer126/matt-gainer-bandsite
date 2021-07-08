c8b93c414019a995f30eb225bde04d8c9beca1b4
Matt Gainer <matt.gainer126@hotmail.com>
2021-07-08 10:25:30 -0400

    updates to functionalty to include api display of comments and updates of comments. This update also includes showing the most current comments first

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -12,6 +12,9 @@ let displayComments = function (comments) {
     let name = document.createElement("p");
     let namediv = document.createElement("div");
     let circle = document.createElement("div");
+    let commentContainer = document.createElement("div");
+    commentContainer.classList.add("comment__container");
+
     circle.classList.add("comment-circle");
     namediv.appendChild(circle);
     name.innerText = element.name;
@@ -20,49 +23,39 @@ let displayComments = function (comments) {
     commentLocal.appendChild(namediv);
     namediv.appendChild(name);
     let date = document.createElement("p");
-    date.innerText = element.date;
+    date.innerText = element.timestamp;
     date.classList.add("comment-display--date");
     namediv.appendChild(date);
     let comment = document.createElement("p");
     comment.innerText = element.comment;
     comment.classList.add("comment-display--comment");
-    commentLocal.appendChild(comment);
+    commentContainer.appendChild(namediv);
+    commentContainer.appendChild(comment);
+    commentLocal.appendChild(commentContainer);
   });
 };
+
+const form = document.getElementById("comment__form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let name = e.target.name.value;
+  let comment = e.target.newcomment.value;
+
+  axios
+    .post("https://project-1-api.herokuapp.com/comments/?api_key=`${key}`", {
+      name: name,
+      comment: comment,
+    })
+    .then((response) => console.log(response.data))
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 axios
   .get("https://project-1-api.herokuapp.com/comments/?api_key=`${key}`")
   .then((response) => {
     let comments = response.data;
+    console.log(comments);
     displayComments(comments);
   });
-
-const form = document.getElementById("comment__form");
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-});
-
-let sendBrowser = document.querySelector(".newcomment");
-let placment = document.querySelector(".comment-display--orientation");
-
-function displayComment() {
-  let name = document.getElementById("comment__form").elements[0].value;
-  let newCommentName = document.createElement("p");
-  newCommentName.innerText = name;
-  newCommentName.classList.add("newComment__name");
-  placment.appendChild(newCommentName);
-  console.log(newCommentName);
-
-  let newDate = document.createElement("p");
-  newDate.innerText = today;
-  newDate.classList.add("newComment__date");
-  placment.appendChild(newDate);
-  console.log(newDate);
-
-  let comment = document.getElementById("comment__form").elements[1].value;
-  let newComment = document.createElement("p");
-  newComment.innerText = comment;
-  newComment.classList.add("newComment__comment");
-  sendBrowser.appendChild(newComment);
-  console.log(newComment);
-}
